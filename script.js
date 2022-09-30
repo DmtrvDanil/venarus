8b52f24bb8c237479b9714344a91d8045f625046
Daniil <dmtrvd90@gmail.com>
2022-10-01 00:10:19 +0300

    animation

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const burgerC = document.querySelector("#burgerC");
 const menu = document.querySelector(".header__menu");
 const navO = document.querySelector(".header__navO");
 const header = document.querySelector(".header__wrapper");
+const wave = document.querySelector(".wave");
+const wave2 = document.querySelector(".wave2");
 console.log(menu);
 
 
@@ -10,9 +12,13 @@ burgerO.addEventListener("click", () => {
         menu.style.display = "block";
         header.style.visibility = "hidden";
         navO.style.visibility = "hidden";
+        wave.style.background = "url(../img/wave2.svg)";
+        wave2.style.display = "none";
 });
 burgerC.addEventListener("click", () => {
     menu.style.display = "none";
     header.style.visibility = "visible";
     navO.style.visibility = "visible";
+    wave.style.background = "url(../img/wave.svg)";
+    wave2.style.display = "block";
 });
